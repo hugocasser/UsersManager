943705405075eee750135af8243358ce79840091
Артём <hugochacasser@gmail.com>
2023-10-06 17:51:11 +0300

    --feature Add Token Services

---
+++ External/Infrastructure/Services/TokenServices/RefreshTokenGeneratorService.cs
@@ -0,0 +1,47 @@
+﻿using Application.Abstraction.Repositories;
+using Application.Abstraction.Services;
+using Application.Common;
+using Application.Exceptions;
+using Domain.Model;
+
+namespace Infrastructure.Services.TokenServices;
+
+public class RefreshTokenGeneratorService : IRefreshTokenGeneratorService
+{
+    private readonly IRefreshTokensRepository _refreshTokensRepository;
+
+    public RefreshTokenGeneratorService(IRefreshTokensRepository refreshTokensRepository)
+    {
+        _refreshTokensRepository = refreshTokensRepository;
+    }
+
+    public RefreshToken GenerateToken(Guid userId)
+    {
+        return new RefreshToken
+        {
+            Id = Guid.NewGuid(),
+            UserId = userId,
+            Token = Utils.GenerateRandomString(7),
+            IsUsed = false,
+            IsRevoked = false,
+            AddedTime = DateTime.UtcNow,
+            ExpiryTime = DateTime.UtcNow.AddMonths(2)
+        };
+    }
+
+    public async Task<RefreshToken> ValidateTokenAsync(Guid userId, string token, CancellationToken cancellationToken)
+    {
+        var refreshToken = await _refreshTokensRepository.FindUserTokenAsync(userId, token, cancellationToken);
+        if (refreshToken is null)
+        {
+            throw new IdentityExceptionWithStatusCode("The refresh token was not generated.");
+        }
+        
+        if (refreshToken.IsRevoked || refreshToken.ExpiryTime < DateTime.UtcNow)
+        {
+            throw new IdentityExceptionWithStatusCode("The refresh token was expired or revoked. Please login again");
+        }
+
+        return refreshToken;
+    }
+}
---
+++ External/Infrastructure/Services/TokenServices/TokenServicesInjection.cs
@@ -0,0 +1,16 @@
+﻿using Application.Abstraction.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure.Services.TokenServices;
+
+public static class TokenServicesInjection
+{
+    public static IServiceCollection UseTokenServices(this IServiceCollection serviceCollection, ITokenGenerationConfiguration configuration)
+    {
+        serviceCollection.AddSingleton(configuration);
+        serviceCollection.AddScoped<IAuthTokenGeneratorService, JwtTokenGeneratorService>();
+        serviceCollection.AddScoped<IRefreshTokenGeneratorService, RefreshTokenGeneratorService>();
+        
+        return serviceCollection;
+    }
+}
