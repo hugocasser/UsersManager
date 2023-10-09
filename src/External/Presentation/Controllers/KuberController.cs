3bf4e3e07e9fc8f21996498614f319eaee9488cc
Артём <hugochacasser@gmail.com>
2023-10-10 00:05:57 +0300

    --feature add migrations and deploy project to k8s

---
+++ src/External/Presentation/Controllers/KuberController.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+using Application.RequestHandlers.Auth.Commands;
+using Application.RequestHandlers.Auth.Commands.ConfirmEmail;
+using Application.RequestHandlers.Auth.Commands.LoginUser;
+using Application.RequestHandlers.Auth.Commands.RefreshToken;
+using Application.RequestHandlers.Auth.Commands.ResendEmailVerificationToken;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.WebUtilities;
+using Presentation.Abstractions;
+
+namespace Presentation.Controllers;
+
+[Route("/health-check")]
+public sealed class KuberController : ApiController
+{
+    public KuberController(ISender sender) : base(sender)
+    {
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> HealthCheck(CancellationToken cancellationToken)
+    {
+        return Ok();
+    }
+}
